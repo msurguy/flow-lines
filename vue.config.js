2762d87ac092410527f17c1959a46c3baf7b8cba
Maksim <msurguy@gmail.com>
2019-11-12 22:00:57 -0800

    build setup

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // publicPath: process.env.NODE_ENV === 'production'
-  //   ? '/flow-lines/'
-  //   : '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/flow-lines/'
+    : '/',
   outputDir: 'docs',
   devServer: {
     port: 8085
