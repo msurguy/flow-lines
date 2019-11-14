4eff9343e0a963e2cec78c689b6a389fee68bf54
Maksim <msurguy@gmail.com>
2019-11-13 21:50:12 -0800

    switch to useSearch instead of hash

--- src/appState.js
+++ src/appState.js
@@ -1,31 +1,30 @@
 import queryState from 'query-state'
 
-export const qs = queryState({
-  xf: 'cos((cos(y) - x * y))', // xFunction
-  yf: 'x', // yFunction
-  dt: 0.005, // dTest
-  sd: 0.2, // separationDistance
-  seed: 1, // seed
-  sm: 0.2, // simplification
-  color: '#000000', // strokeColor
-  bg: null, // backgroundColor
-  ts: 0.05, // timeStep
-  sw: 1 // strokeWidth
-}, {
-  useSearch: true
-})
+export const qs = queryState({}, { useSearch: true })
 
 const currentStateFromQuery = qs.get()
 
+export const defaults = {
+  xFunction: 'cos((cos(y) - x * y))',
+  yFunction: 'x',
+  dTest: 0.005,
+  separationDistance: 0.2,
+  simplification: 0.2,
+  color: '#000000',
+  bg: null,
+  timeStep: 0.05,
+  strokeWidth: 1
+}
+
 export const appState = {
-  xFunction: currentStateFromQuery.xf,
-  yFunction: currentStateFromQuery.yf,
-  seed: currentStateFromQuery.seed,
-  color: currentStateFromQuery.color,
-  dTest: currentStateFromQuery.dt,
-  separationDistance: currentStateFromQuery.sd, // Separation distance between new streamlines.
-  simplification: currentStateFromQuery.sm, // line simplification amount (0.1-2)
-  timeStep: currentStateFromQuery.ts,
-  bg: currentStateFromQuery.bg,
-  strokeWidth: currentStateFromQuery.sw
+  xFunction: currentStateFromQuery.xf || defaults.xFunction,
+  yFunction: currentStateFromQuery.yf || defaults.yFunction,
+  seed: currentStateFromQuery.seed || 1,
+  color: currentStateFromQuery.color || defaults.color,
+  dTest: currentStateFromQuery.dt || defaults.dTest,
+  separationDistance: currentStateFromQuery.sd || defaults.separationDistance, // Separation distance between new streamlines.
+  simplification: currentStateFromQuery.sm || defaults.simplification, // line simplification amount (0.1-2)
+  timeStep: currentStateFromQuery.ts || defaults.timeStep,
+  bg: currentStateFromQuery.bg || defaults.bg,
+  strokeWidth: currentStateFromQuery.sw || defaults.strokeWidth
 }
